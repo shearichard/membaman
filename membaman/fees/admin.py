7d011a210fea78e08fa31f5de6897288f546292e
Richard Shea <rshea@thecubagroup.com>
2014-08-30 20:56:49 +1200

    Allowing fees models to be seen in admin

--- membaman/fees/admin.py
+++ membaman/fees/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from fees.models import Year
+from fees.models import SubYear 
+from fees.models import Income
+
+admin.site.register(Year)
+admin.site.register(SubYear)
+admin.site.register(Income)
