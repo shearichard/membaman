b3446c3e2aebc05b78e0b7fb8a556b03472c9bcd
Richard Shea <rshea@thecubagroup.com>
2014-09-03 12:25:13 +1200

    Fixing some typos in the boilerplate Django Custom Command

--- membaman/members/management/commands/examplecommand.py
+++ membaman/members/management/commands/examplecommand.py
@@ -3,13 +3,13 @@ from members.models import Organisation
 
 class Command(BaseCommand):
     args = '<org_id org_id ...>'
-    help = 'Closes the specified poll for voting'
+    help = 'Outputs the name of the specified Organisation'
 
     def handle(self, *args, **options):
         for org_id in args:
             try:
-                poll = Organisation.objects.get(pk=int(org_id))
+                org = Organisation.objects.get(pk=int(org_id))
             except Organisation.DoesNotExist:
                 raise CommandError('Organisation "%s" does not exist' % org_id)
 
-            self.stdout.write('Org Name =  "%s"' % poll.name)
+            self.stdout.write('Org Name =  "%s"' % org.name)
