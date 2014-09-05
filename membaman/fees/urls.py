0fa3c3ca90d695713bf46bdd650aa963693546a9
Richard Shea <rshea@thecubagroup.com>
2014-09-05 23:00:30 +1200

    First, very barebones, non-admin views for fees app

---
+++ membaman/fees/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from . import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.IncomeListView.as_view(), name='income-list'),
+)
--- membaman/fees/views.py
+++ membaman/fees/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView, UpdateView
+
+from django.core.urlresolvers import reverse
+
+from .models import Year, SubYear, Income
+
+class IncomeListView(ListView):
+    model = Income
+    template_name = 'fees/income_list_simple.html'
+    context_object_name = "income_list"
+    paginate_by = 10
+
