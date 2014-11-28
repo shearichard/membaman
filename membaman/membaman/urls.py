bed3ffbf1dac1ef364c10a257a6fa672421351e8
Richard Shea <rshea@thecubagroup.com>
2014-11-29 00:14:11 +1300

    Make some changes to the default titling of Admin pages

--- membaman/membaman/urls.py
+++ membaman/membaman/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
 from django.views.generic.base import TemplateView
+admin.site.site_header = 'MembaMan Administration Area'
+admin.site.site_title = 'MembaMan Admin'
 
 urlpatterns = patterns('',
     # Examples:
