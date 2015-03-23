690680f749538833182d487b05a1afcc7635d649
Richard Shea <rshea@thecubagroup.com>
2015-03-24 11:02:05 +1300

    Push queryset functionality for Member listviews up into superclass

--- membaman/members/views.py
+++ membaman/members/views.py
@@ -7,28 +7,23 @@ from .models import Family, Caregiver, Member, Person
 TEMP_ORG_NAME = 'Conversion Group'
 TEMP_ORG_ID = 45 
 
-class MemberActiveListView(ListView):
+class MemberListView(ListView):
     model = Member
-    template_name = 'fees/member_list.html'
+    template_name = 'members/member_list.html'
     context_object_name = "member_list"
     paginate_by = 100
 
-    def get_queryset(self):
-        mem_active = Member.objects.filter(no_longer_attends=False)
-        lst_mem_active = sorted(mem_active,  key=lambda m: m.last_first_name)
-        return lst_mem_active
+    def get_queryset(self, active):
+        mem = Member.objects.filter(no_longer_attends=active)
+        return mem
 
-class MemberNotActiveListView(ListView):
-    model = Member
-    template_name = 'fees/member_list.html'
-    context_object_name = "member_list"
-    paginate_by = 100
 
+class MemberActiveListView(MemberListView):
     def get_queryset(self):
-        '''
-        Sortation shown here derived from : http://stackoverflow.com/a/4175785/364088
-        '''
-        mem_not_active = Member.objects.filter(no_longer_attends=True)
-        lst_mem_not_active = sorted(mem_not_active,  key=lambda m: m.last_first_name)
-        return mem_not_active
+        return super(MemberActiveListView, self).get_queryset(active=False)
+
+
+class MemberNotActiveListView(MemberListView):
+    def get_queryset(self):
+        return super(MemberNotActiveListView, self).get_queryset(active=True)
 
